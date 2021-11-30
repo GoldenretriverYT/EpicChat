a3245d17e59c7bfda3914735cb26df0c9225b40e
GoldenretriverYT <49924528+GoldenretriverYT@users.noreply.github.com>
2021-11-30 14:48:46 +0100

    Update chatConfig.json.js

--- server/chatConfig.json.js
+++ server/chatConfig.json.js
@@ -1,10 +1,10 @@
 module.exports = {
-    "allowBold": true,
-    "allowImages": true,
-    "allowLinks": true, // This will not block links, but will disable/enable highlighting of links when using [!LINK||TEXT] syntax
-    "maxMessageLength": 2000,
-    "maxUsernameLength": 20,
-    "adminPassword": "example123", // Change this password!
-    "port": 6060,
-    "maxFileSizeKB": 16384 // Files are currently exclusively stored in RAM - do not make this limit too high or you will get an OOM error
+    "allowBold": true, // Toggles the functionality to write text bold with this syntax: *my bold text*
+    "allowImages": true, // Toggles the functionality to embed imaged with this syntax: [!LINK TO IMAGE]
+    "allowLinks": true, // This will not block links, but will disable/enable highlighting of links when using [~LINK||TEXT] syntax
+    "maxMessageLength": 2000, // How long can the message be?
+    "maxUsernameLength": 20, // How long can the username be?
+    "adminPassword": "example123", // VERY IMPORTANT => Change this password!
+    "port": 6060, // The port the server should run on
+    "maxFileSizeKB": 16384 // Max file size in kilobytes - files are currently exclusively stored in RAM - do not make this limit too high or you will get an OOM error
 }
