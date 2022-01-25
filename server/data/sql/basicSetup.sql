2ff31fa1f02d31e480bd9e94ef426b709c48e4bb
GoldenretriverYT <49924528+GoldenretriverYT@users.noreply.github.com>
2022-01-25 09:58:51 +0100

    yy

---
+++ server/data/sql/basicSetup.sql
@@ -0,0 +1,124 @@
+-- MySQL dump 10.13  Distrib 8.0.27, for Win64 (x86_64)
+--
+-- Host: localhost    Database: epicchat
+-- ------------------------------------------------------
+-- Server version	8.0.27
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+CREATE DATABASE IF NOT EXISTS epicchat;
+SELECT epicchat;
+--
+-- Table structure for table `info`
+--
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE IF NOT EXISTS`info` (
+  `key` text,
+  `val` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `info`
+--
+
+LOCK TABLES `info` WRITE;
+/*!40000 ALTER TABLE `info` DISABLE KEYS */;
+INSERT INTO `info` VALUES ('structVersion','1');
+/*!40000 ALTER TABLE `info` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `messages`
+--
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE IF NOT EXISTS`messages` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `authorId` int DEFAULT NULL,
+  `content` mediumtext,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `messages`
+--
+
+LOCK TABLES `messages` WRITE;
+/*!40000 ALTER TABLE `messages` DISABLE KEYS */;
+/*!40000 ALTER TABLE `messages` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `roles`
+--
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE IF NOT EXISTS`roles` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` text,
+  `color` tinytext,
+  `permissionWrite` tinyint DEFAULT NULL,
+  `permissionDeleteOther` tinyint DEFAULT NULL,
+  `permissionBan` tinyint DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `roles`
+--
+
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE IF NOT EXISTS`users` (
+  `userId` int NOT NULL AUTO_INCREMENT,
+  `username` text,
+  `passwordHash` text,
+  `assignedRole` int DEFAULT NULL,
+  PRIMARY KEY (`userId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-01-25  9:56:25
--- server/server.js
+++ server/server.js
@@ -1,193 +1,168 @@
-const express = require('express');
-const http = require('http');
-const fs = require("fs");
-const bcrypt = require("bcrypt");
-const { Server } = require("socket.io");
+(async () => {
+    const express = require('express');
+    const http = require('http');
+    const fs = require("fs");
+    const bcrypt = require("bcrypt");
+    const { Server } = require("socket.io");
 
-var config = require("./chatConfig.json");
+    var config = require("./chatConfig.json");
 
-/*
-  Users Struct:
-    {
-        userId: {
-            "username": "<username>",
-            "passwordHash": "<bcrypt password hash>",
-            "role": "<roleId>",
-            "authToken": "",
-        }
-    }
-*/
-if(!fs.existsSync("users.json")) fs.writeFileSync("users.json", "{}");
-var users = JSON.parse(fs.readFileSync("users.json"));
-
-/*
-  Roles Struct:
-    {
-        roleId: {
-            "roleName": "<role name>",
-            "roleColor": "<hex color>",
-            "permissions": ["write", "ban"]
-        }
-    }
-*/
-if(!fs.existsSync("roles.json")) fs.writeFileSync("roles.json", `[{
-    "roleId": "0",
-    "roleName": "everyone",
-    "roleColor": "#000000",
-    "permissions": ["write"]
-}]`);
-var roles = JSON.parse(fs.readFileSync("roles.json"));
-
-/*
-    Messages Struct:
-    [
-        {
-            "messageId": 0,
-            "writtenBy": "<userId>",
-            "content": "<content>",
-            "writtenAt": "<timestamp>",
-        }
-    ]
-*/
-if(!fs.existsSync("messages.json")) fs.writeFileSync("messages.json", "[]");
-var messages = JSON.parse(fs.readFileSync("messages.json"));
-
-/* Setting up web server & socket.io */
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server, {
-    "pingTimeout": 120*1000,
-    "maxHttpBufferSize": (1024*1024) + (config.maxFileSizeKB*1024)
-});
-
-app.use('/', express.static('public'));
-
-io.on('connection', (socket) => {
-    socket.uniqueId = generateUID();
-    socket.authToken = "not_logged_in";
-
-    socket.on("update auth token", (data, answer) => {
-        socket.authToken = data;
-        answer({success: 1, data: {}});
+    /* Setting up web server & socket.io */
+    const app = express();
+    const server = http.createServer(app);
+    const io = new Server(server, {
+        "pingTimeout": 120*1000,
+        "maxHttpBufferSize": (1024*1024) + (config.maxFileSizeKB*1024)
     });
 
-    socket.on("verify auth token", (data, answer) => {
-        if(getUserBySocket(socket) == null) {
-            answer({success: 0, data: "No matching user account was found"});
-        }else {
-            answer({success: 1, data: ""});
-        }
+    const mysql = require('mysql2/promise');
+
+    const connection = await mysql.createConnection({
+        host: config.data.host,
+        user: config.data.user,
+        password: config.data.pass
+    }).catch((err) => {
+        exitErr("Failed to connect to database: " + err);
     });
 
-    socket.on("create account", (data, answer) => {
-        if(!(username in data) || !(password in data)) {
-            answer({success: 0, data: "No username/password provided"});
-            return;
-        }
+    const [rows, fields] = await connection.query(fs.readFileSync("data/sql/basicSetup.sql")).catch((err) => {
+        exitErr("Failed to setup database: " + err);
+    });
 
-        if(data.username.length < 3 || data.user.length > 20) {
-            answer({success: 0, data: "Username too short or too long. (3-20 chars)"});
-            return;
-        }
+    app.use('/', express.static('public'));
 
-        if(data.username.match(/([^A-Za-z0-9_])/g).length > 0) {
-            answer({success: 0, data: "Username includes invalid characters (allowed: a-zA-Z0-9_)"});
-            return;
-        }
+    io.on('connection', (socket) => {
+        socket.uniqueId = generateUID();
+        socket.authToken = "not_logged_in";
 
-        if(data.password.length < 8) {
-            answer({success: 0, data: "Your password is too short. At least 8 characters."});
-            return;
-        }
-
-        bcrypt.hash(data.password).then((hash) => {
-            users[generateUID(16)] = ({
-                "username": data.username,
-                "passwordHash": hash,
-                "role": "0",
-                "authToken": "NOT_LOGGED_IN",
-            });
-
-            answer({success: 1, data: "You can now log in."});
+        socket.on("update auth token", (data, answer) => {
+            socket.authToken = data;
+            answer({success: 1, data: {}});
         });
-    });
 
-    socket.on("login account", (data, answer) => {
-        if(!(username in data) || !(password in data)) {
-            answer({success: 0, data: "No username/password provided"});
-            return;
-        }
-
-        if(data.username.length < 3 || data.user.length > 20) {
-            answer({success: 0, data: "Username too short or too long. (3-20 chars)"});
-            return;
-        }
-
-        if(data.username.match(/([^A-Za-z0-9_])/g).length > 0) {
-            answer({success: 0, data: "Username includes invalid characters (allowed: a-zA-Z0-9_)"});
-            return;
-        }
-
-        var user = getUserBy("username", data.username);
-
-        if(user == null) {
-            answer(fail("Username or password invalid."));
-            return;
-        }
-
-        bcrypt.compare(data.password, users[user].passwordHash).then((isValid) => {
-            if(isValid) {
-                users[user].authToken = generateUID(16);
-                socket.authToken = users[user].authToken;
-
-                answer(success(users[user].authToken)); // Also send to user to save auth token for later resend
+        socket.on("verify auth token", (data, answer) => {
+            if(getUserBySocket(socket) == null) {
+                answer({success: 0, data: "No matching user account was found"});
             }else {
-                answer(fail("Username or password invalid."));
+                answer({success: 1, data: ""});
             }
         });
+
+        socket.on("create account", (data, answer) => {
+            if(!(username in data) || !(password in data)) {
+                answer({success: 0, data: "No username/password provided"});
+                return;
+            }
+
+            if(data.username.length < 3 || data.user.length > 20) {
+                answer({success: 0, data: "Username too short or too long. (3-20 chars)"});
+                return;
+            }
+
+            if(data.username.match(/([^A-Za-z0-9_])/g).length > 0) {
+                answer({success: 0, data: "Username includes invalid characters (allowed: a-zA-Z0-9_)"});
+                return;
+            }
+
+            if(data.password.length < 8) {
+                answer({success: 0, data: "Your password is too short. At least 8 characters."});
+                return;
+            }
+
+            bcrypt.hash(data.password).then((hash) => {
+                users[generateUID(16)] = ({
+                    "username": data.username,
+                    "passwordHash": hash,
+                    "role": "0",
+                    "authToken": "NOT_LOGGED_IN",
+                });
+
+                answer({success: 1, data: "You can now log in."});
+            });
+        });
+
+        socket.on("login account", (data, answer) => {
+            if(!(username in data) || !(password in data)) {
+                answer({success: 0, data: "No username/password provided"});
+                return;
+            }
+
+            if(data.username.length < 3 || data.user.length > 20) {
+                answer({success: 0, data: "Username too short or too long. (3-20 chars)"});
+                return;
+            }
+
+            if(data.username.match(/([^A-Za-z0-9_])/g).length > 0) {
+                answer({success: 0, data: "Username includes invalid characters (allowed: a-zA-Z0-9_)"});
+                return;
+            }
+
+            var user = getUserBy("username", data.username);
+
+            if(user == null) {
+                answer(fail("Username or password invalid."));
+                return;
+            }
+
+            bcrypt.compare(data.password, users[user].passwordHash).then((isValid) => {
+                if(isValid) {
+                    users[user].authToken = generateUID(16);
+                    socket.authToken = users[user].authToken;
+
+                    answer(success(users[user].authToken)); // Also send to user to save auth token for later resend
+                }else {
+                    answer(fail("Username or password invalid."));
+                }
+            });
+        });
     });
-});
 
-server.listen(config.port, () => {
-    console.log(`EpicChat is now listening to port ${config.port}`);
-});
+    server.listen(config.port, () => {
+        console.log(`EpicChat is now listening to port ${config.port}`);
+    });
 
-var alp = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    var alp = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
-function generateUID(len = 8) {
-    var res = "";
+    function generateUID(len = 8) {
+        var res = "";
 
-    for(var i = 0; i < len; i++) {
-        res += alp[Math.floor(Math.random() * alp.length)];
+        for(var i = 0; i < len; i++) {
+            res += alp[Math.floor(Math.random() * alp.length)];
+        }
+
+        return res;
     }
 
-    return res;
-}
-
-function getUserBySocket(socket) {
-    for(var user of Object.keys(users)) {
-        if(users[user].authToken == socket.authToken) {
-            return user;
+    function getUserBySocket(socket) {
+        for(var user of Object.keys(users)) {
+            if(users[user].authToken == socket.authToken) {
+                return user;
+            }
         }
     }
-}
 
-function getUserBy(field, value) {
-    for(var user of Object.keys(users)) {
-        if(field == "id" && user == value) return user;
+    function getUserBy(field, value) {
+        for(var user of Object.keys(users)) {
+            if(field == "id" && user == value) return user;
 
-        if(field in user && user[field] == value) return user;
+            if(field in user && user[field] == value) return user;
+        }
     }
-}
 
-function hasPermission(user, permission) {
-    return permission in roles[user.role].permissions;
-}
+    function hasPermission(user, permission) {
+        return permission in roles[user.role].permissions;
+    }
 
-function fail(data) {
-    return {success: 0, data: data};
-}
+    function fail(data) {
+        return {success: 0, data: data};
+    }
 
-function success(data) {
-    return {success: 1, data: data};
-}
+    function success(data) {
+        return {success: 1, data: data};
+    }
+
+    function exitErr(err) {
+        console.error(err);
+        process.exit(1);
+    }
+})();
