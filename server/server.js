27f41d5dc0ae03a38a62a579fa3e379e3726a00b
GoldenretriverYT <49924528+GoldenretriverYT@users.noreply.github.com>
2022-01-25 08:39:58 +0100

    ee

--- server/server.js
+++ server/server.js
@@ -31,6 +31,14 @@ var users = JSON.parse(fs.readFileSync("users.json"));
 */
 var roles = JSON.parse(fs.readFileSync("roles.json"));
 
+if(!"0" in roles) {
+    roles["0"] = {
+        "roleName": "everyone",
+        "roleColor": "#000000",
+        "permissions": ["write"]
+    }
+}
+
 /*
     Messages Struct:
     [
@@ -69,7 +77,7 @@ io.on('connection', (socket) => {
     });
 
     socket.on("verify auth token", (data, answer) => {
-        if(getUserIdBySocket(socket) == null) {
+        if(getUserBySocket(socket) == null) {
             answer({success: 0, data: "No matching user account was found"});
         }else {
             answer({success: 1, data: ""});
@@ -92,17 +100,57 @@ io.on('connection', (socket) => {
             return;
         }
 
-        if(data.password < 8) {
+        if(data.password.length < 8) {
             answer({success: 0, data: "Your password is too short. At least 8 characters."});
             return;
         }
 
         bcrypt.hash(data.password).then((hash) => {
             users[generateUID()] = {
-                
+                "username": data.username,
+                "passwordHash": hash,
+                "role": "0",
+                "authToken": "NOT_LOGGED_IN",
+            }
+
+            answer({success: 1, data: "You can now log in."});
+        });
+    });
+
+    socket.on("login account", (data, answer) => {
+        if(!(username in data) || !(password in data)) {
+            answer({success: 0, data: "No username/password provided"});
+            return;
+        }
+
+        if(data.username.length < 3 || data.user.length > 20) {
+            answer({success: 0, data: "Username too short or too long. (3-20 chars)"});
+            return;
+        }
+
+        if(data.username.match(/([^A-Za-z0-9_])/g).length > 0) {
+            answer({success: 0, data: "Username includes invalid characters (allowed: a-zA-Z0-9_)"});
+            return;
+        }
+
+        var user = getUserBy("username", data.username);
+
+        if(user == null) {
+            answer(fail("Username or password invalid."));
+            return;
+        }
+
+        bcrypt.compare(data.password, users[user].passwordHash).then((isValid) => {
+            if(isValid) {
+                users[user].authToken = generateUID(16);
+                socket.authToken = users[user].authToken;
+
+                answer(success(users[user].authToken)); // Also send to user to save auth token for later resend
+            }else {
+                answer(fail("Username or password invalid."));
             }
         });
-    })
+    });
 });
 
 server.listen(config.port, () => {
@@ -121,7 +169,7 @@ function generateUID(len = 8) {
     return res;
 }
 
-function getUserIdBySocket(socket) {
+function getUserBySocket(socket) {
     for(var user of Object.keys(users)) {
         if(users[user].authToken == socket.authToken) {
             return user;
@@ -129,6 +177,22 @@ function getUserIdBySocket(socket) {
     }
 }
 
+function getUserBy(field, value) {
+    for(var user of Object.keys(users)) {
+        if(field == "id" && user == value) return user;
+
+        if(field in user && user[field] == value) return user;
+    }
+}
+
 function hasPermission(user, permission) {
     return permission in roles[user.role].permissions;
+}
+
+function fail(data) {
+    return {success: 0, data: data};
+}
+
+function success(data) {
+    return {success: 1, data: data};
 }
